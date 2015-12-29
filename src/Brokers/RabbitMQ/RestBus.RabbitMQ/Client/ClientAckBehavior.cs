7d8ddb0e764e2a718c1d6eb390e73f1e597723cb
Sunny Ahuwanya <sahuwanya@gmail.com>
2015-12-29 10:09:54 -0500

    Introduced ClientSettings and ClientAckBehavior.

---
+++ src/Brokers/RabbitMQ/RestBus.RabbitMQ/Client/ClientAckBehavior.cs
@@ -0,0 +1,18 @@
+﻿namespace RestBus.RabbitMQ.Client
+{
+    public enum ClientAckBehavior
+    {
+        /// <summary>
+        /// All responses received by the client are automatically considered acknowledged by the server.
+        /// </summary>
+        Automatic,
+
+        /// <summary>
+        /// Responses expected by the client in the valid format are explicitly acknowledged. Other responses are rejected.
+        /// </summary>
+        /// <remarks>
+        /// This option is only effective when <see cref="ClientSettings.DisableDirectReplies"/> is true.
+        /// </remarks>
+        ValidMessages
+    }
+}
---
+++ src/Brokers/RabbitMQ/RestBus.RabbitMQ/Client/ClientSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestBus.RabbitMQ.Client
+{
+    public class ClientSettings
+    {
+        public ClientAckBehavior AckBehavior { get; set; }
+        public bool DisableDirectReplies { get; set; }
+    }
+}
