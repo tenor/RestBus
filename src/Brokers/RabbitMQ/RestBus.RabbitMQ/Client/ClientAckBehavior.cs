6ca97d49212ddda0b2003315eff2a8b90937f00d
Sunny Ahuwanya <sahuwanya@gmail.com>
2015-12-29 13:06:06 -0500

    Renamed ClientAckBehavior.ValidMessages to ValidResponses.

--- src/Brokers/RabbitMQ/RestBus.RabbitMQ/Client/ClientAckBehavior.cs
+++ src/Brokers/RabbitMQ/RestBus.RabbitMQ/Client/ClientAckBehavior.cs
@@ -3,16 +3,16 @@
     public enum ClientAckBehavior
     {
         /// <summary>
-        /// All responses received by the client are automatically considered acknowledged by the server.
+        /// All responses received by the client are automatically acknowledged by the broker.
         /// </summary>
         Automatic,
 
         /// <summary>
-        /// Responses expected by the client in the valid format are explicitly acknowledged. Other responses are rejected.
+        /// Responses expected by the client in valid format are explicitly acknowledged. Other responses are rejected.
         /// </summary>
         /// <remarks>
         /// This option is only effective when <see cref="ClientSettings.DisableDirectReplies"/> is true.
         /// </remarks>
-        ValidMessages
+        ValidResponses
     }
 }
