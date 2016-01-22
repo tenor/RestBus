21a99d4ed94ce2ec738d01e53a2a6ff9c18f184c
Sunny Ahuwanya <sahuwanya@gmail.com>
2016-01-21 23:29:20 -0500

    Added notes on a new AfterResponse SubscriberAckBehavior flag.

--- src/Brokers/RabbitMQ/RestBus.RabbitMQ/Subscription/SubscriberAckBehavior.cs
+++ src/Brokers/RabbitMQ/RestBus.RabbitMQ/Subscription/SubscriberAckBehavior.cs
@@ -14,6 +14,8 @@
         /// </summary>
         Automatic
 
+        //NOTE: If adding a new entry, consider making this a [Flags] enumeration to support the combinable AfterResponse flag. (see below)
+
         /*
         Possible future additions, for consideration, to ack behavior include:
 
@@ -31,6 +33,18 @@
                                      Or decide if to defer acknowledgement to a CustomAfterProcessing delegate.
 
             CustomAfterProcessed -- You supply your own delegate that decides if a request should be acknowledged or reject after processing is complete.
+
+            AfterResponse -- Send Ack after sending response (and confirming response was received, if publisher confirms is turned on)
+                             Can be combined with ProcessedRequests, ProcessedErrorFree, CustomAfterProcessed
+                             There is an increased risk of a race condition with this flag in the scenario where:
+                             Subscriber sends a response and then crashes unexpectedly.
+                             The client receives the response and carries on
+                             The subscriber comes back online and the broker redelivers the message and the subscriber reprocesses it.
+                             The client then receives a duplicate response.
+                             This could also occur in ordinary ProcessedRequests mode where the subscriber crashes 
+                             after processing the request but just before acking it, but is less likely because requests are acked immediately
+                             after processing.
+                             In the ProcessedRequests mode, the client can only receive a maximum of one response for each request.
 
         */
     }
