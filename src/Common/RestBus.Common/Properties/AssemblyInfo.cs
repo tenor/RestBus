fcea2a2f3ec3d318fa4a23ca8cd1c2e1d6d10feb
Sunny Ahuwanya <sahuwanya@gmail.com>
2016-01-25 19:42:36 -0500

    Increased R.Common version to 0.6.3.

--- src/Common/RestBus.Common/Properties/AssemblyInfo.cs
+++ src/Common/RestBus.Common/Properties/AssemblyInfo.cs
@@ -22,6 +22,6 @@
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.6.2.0")]
-[assembly: AssemblyFileVersion("0.6.2.0")]
-[assembly: AssemblyInformationalVersion("0.6.2")]
+[assembly: AssemblyVersion("0.6.3.0")]
+[assembly: AssemblyFileVersion("0.6.3.0")]
+[assembly: AssemblyInformationalVersion("0.6.3")]
